82a48b6894f8d197fe9e76a6e26a85a07faaa5b2
peiboning <peiboning@peiboningdeMacBook-Pro.local>
2017-09-06 18:48:14 +0800

    add code

---
+++ app/src/main/java/com/rn/android/MyApplication.java
@@ -0,0 +1,17 @@
+package com.rn.android;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+/**
+ * Created by peiboning on 2017/9/6.
+ */
+
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/rn/android/PermissionActivity.java
@@ -0,0 +1,44 @@
+package com.rn.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+public class PermissionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 300);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 300) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                }
+            }
+        }
+    }
+
+
+    public void test(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
